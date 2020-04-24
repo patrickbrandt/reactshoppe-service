e838983c9e68da3e14ee13c13bc85c8a8b6b3971
Patrick Brandt <patrick@fullstory.com>
2020-04-24 19:57:05 -0400

    Initial commit

---
+++ bin/reactshoppe-service.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { ReactshoppeServiceStack } from '../lib/reactshoppe-service-stack';
+
+const app = new cdk.App();
+new ReactshoppeServiceStack(app, 'ReactshoppeServiceStack');
